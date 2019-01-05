ee7f6f322fd705d1d4d6c95ae53182d8215148be
dhope0000 <dhope0000@gmail.com>
2019-01-05 21:41:04 +0000

    Added a use statement to the 0.1.0 mysql

--- sql/0.1.0.sql
+++ sql/0.1.0.sql
@@ -1,3 +1,5 @@
+USE `LXD_Manager`;
+
 ALTER TABLE `Hosts`
     ADD `Host_Cert_Only_File` VARCHAR(255) NOT NULL AFTER `Host_Cert_Path`,
-    ADD `Host_Key_File` VARCHAR(255) NOT NULL AFTER `Host_Cert_Only_File`; 
+    ADD `Host_Key_File` VARCHAR(255) NOT NULL AFTER `Host_Cert_Only_File`;
