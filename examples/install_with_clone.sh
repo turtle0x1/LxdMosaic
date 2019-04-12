e15df6a987b5781e843de982b7acb5211d0ca9c5
turtle0x1 <12494629+turtle0x1@users.noreply.github.com>
2019-04-12 22:36:36 +0100

    Remove branch checkout from install script

--- examples/install_with_clone.sh
+++ examples/install_with_clone.sh
@@ -41,8 +41,6 @@ chown -R www-data:www-data /var/www/LxdMosaic/src/sensitiveData/certs
 # Move in LxdManager
 cd /var/www/LxdMosaic
 
-git checkout detect-offline
-
 npm install
 
 # Install Dependecies
