6cfe885defb59f995fd8dcdb7dc1414b609d9fb7
dhope0000 <dhope0000@gmail.com>
2019-04-21 18:32:08 +0100

    Added an example deploy (may remove before production)

--- examples/install_with_clone.sh
+++ examples/install_with_clone.sh
@@ -38,6 +38,8 @@ chown -R www-data:www-data /var/www/LxdMosaic/src/sensitiveData/certs
 # Move in LxdManager
 cd /var/www/LxdMosaic || exit
 
+git checkout deployments
+
 npm install
 
 # Install Dependecies
@@ -62,6 +64,7 @@ mysql < sql/instance_types.sql
 mysql < sql/store_details.sql
 mysql < sql/host_alias.sql
 mysql < sql/host_online.sql
+mysql < sql/deployments.sql
 
 
 cp examples/lxd_manager.conf /etc/apache2/sites-available/
--- sql/deployments.sql
+++ sql/deployments.sql
@@ -18,3 +18,33 @@ FOREIGN KEY `drop_on_delete`(`DCC_Deployment_ID`)
 REFERENCES `Deployments`(`Deployment_ID`)
 ON DELETE CASCADE
 ON UPDATE CASCADE;
+
+
+
+-- Demo example
+
+INSERT INTO `Cloud_Config` (`CC_ID`, `CC_Name`, `CC_Namespace`, `CC_Description`) VALUES (1, "Example Node", "example/node", "an example cloud config for a node app");
+
+INSERT INTO `Cloud_Config_Data` (`CCD_ID`, `CCD_Cloud_Config_ID`, `CCD_Data`) VALUES (1, 1, "#cloud-config
+
+# Apply updates using apt
+package_update: true
+package_upgrade: true
+
+# Install packages
+packages:
+ - nodejs
+ - npm
+
+runcmd:
+ - git clone https://github.com/turtle0x1/nodeExample /root/nodeExample
+ - cd /root/nodeExample && npm install
+ - npm -g install pm2
+ - pm2 start /root/nodeExample/index.js
+ - pm2 startup
+ - pm2 save");
+
+INSERT INTO `Deployments` (`Deployment_ID`, `Deployment_Name`) VALUES (1, "Example");
+
+INSERT INTO `Deployment_Cloud_Config` (`DCC_Deployment_ID`, `DCC_Cloud_Config_Rev_ID`)
+VALUES (1, 1);
