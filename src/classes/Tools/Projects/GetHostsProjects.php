2ee9df1e479f76fe390396d0d4d837cc508ef70d
turtle0x1 <dhope0000@gmail.com>
2019-05-04 17:23:14 +0100

    Fix Cant view projects if one host doesn't support projects (close #76)

--- src/classes/Tools/Projects/GetHostsProjects.php
+++ src/classes/Tools/Projects/GetHostsProjects.php
@@ -4,13 +4,18 @@ namespace dhope0000\LXDClient\Tools\Projects;
 
 use dhope0000\LXDClient\Model\Client\LxdClient;
 use dhope0000\LXDClient\Model\Hosts\HostList;
+use dhope0000\LXDClient\Tools\Hosts\HasExtension;
 
 class GetHostsProjects
 {
-    public function __construct(HostList $hostList, LxdClient $lxdClient)
-    {
+    public function __construct(
+        HostList $hostList,
+        LxdClient $lxdClient,
+        HasExtension $hasExtension
+    ) {
         $this->hostList = $hostList;
         $this->lxdClient = $lxdClient;
+        $this->hasExtension = $hasExtension;
     }
 
     public function getAll()
@@ -26,6 +31,11 @@ class GetHostsProjects
                 continue;
             }
             $client = $this->lxdClient->getANewClient($host["Host_ID"]);
+
+            if(!$this->hasExtension->checkWithClient($client, "projects")){
+                continue;
+            }
+
             $projects = $client->projects->all();
 
 
