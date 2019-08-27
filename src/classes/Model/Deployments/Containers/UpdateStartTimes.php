415082e4e37aea37712f08a5b6dc339c0a729dd1
turtle0x1 <dhope0000@gmail.com>
2019-08-27 23:08:50 +0100

    Store first start time & last start time of deployment containers
    The first time is quite rough at the moment as it only records the
    first time LXDMosaic starts the container (might have to investigate
    querying LXD to see if it has first start time some where), it
    also records the last time LXDMosaic starts the container (if started
    through the deployment)

---
+++ src/classes/Model/Deployments/Containers/UpdateStartTimes.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace dhope0000\LXDClient\Model\Deployments\Containers;
+
+use dhope0000\LXDClient\Model\Database\Database;
+
+class UpdateStartTimes
+{
+    public function __construct(Database $database)
+    {
+        $this->database = $database->dbObject;
+    }
+
+    public function updateFirstStart(int $deploymentId, int $hostId, string  $name)
+    {
+        $sql = "UPDATE
+                    `Deployment_Containers`
+                SET
+                    `DC_First_Start` = CURRENT_TIMESTAMP
+                WHERE
+                    `DC_Host_ID` = :hostId
+                AND
+                    `DC_Deployment_ID` = :deploymentId
+                AND
+                    `DC_Name` = :name
+                AND
+                    `DC_First_Start` IS NULL -- Do this to prevent adding new dates
+                ";
+        $do = $this->database->prepare($sql);
+        $do->execute([
+            ":hostId"=>$hostId,
+            ":deploymentId"=>$deploymentId,
+            ":name"=>$name
+        ]);
+        return $do->rowCount() ? true : false;
+    }
+
+    public function updateLastStart(int $deploymentId, int $hostId, string  $name)
+    {
+        $sql = "UPDATE
+                    `Deployment_Containers`
+                SET
+                    `DC_Last_Start` = CURRENT_TIMESTAMP
+                WHERE
+                    `DC_Host_ID` = :hostId
+                AND
+                    `DC_Deployment_ID` = :deploymentId
+                AND
+                    `DC_Name` = :name
+                ";
+        $do = $this->database->prepare($sql);
+        $do->execute([
+            ":hostId"=>$hostId,
+            ":deploymentId"=>$deploymentId,
+            ":name"=>$name
+        ]);
+        return $do->rowCount() ? true : false;
+    }
+}
