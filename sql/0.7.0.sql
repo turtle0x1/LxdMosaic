294620dd3d95cbb6d4607b67eafe3aaa4dbfdc0f
turtle0x1 <dhope0000@gmail.com>
2020-04-12 01:12:21 +0100

    Combine sql files

--- sql/0.7.0.sql
+++ sql/0.7.0.sql
@@ -1,5 +1,7 @@
 USE `LXD_Manager`;
 
+ALTER TABLE `Fleet_Analytics` ADD COLUMN `FA_Total_Storage_Available` BIGINT NOT NULL;
+
 CREATE TABLE `Users` (
     `User_ID` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `User_Date_Created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -8,6 +10,13 @@ CREATE TABLE `Users` (
     `User_Admin` TINYINT DEFAULT 0
 );
 
+CREATE TABLE `User_Api_Tokens` (
+    `UAT_ID` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `UAT_Created_At` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `UAT_User_ID` INT(11),
+    `UAT_Token` TEXT NOT NULL,
+    `UAT_Permanent` TINYINT NOT NULL DEFAULT 0
+);
 
 INSERT INTO `Users` (`User_ID`, `User_Name`, `User_Password`, `User_Admin`) VALUES
     (1, "admin", "$2y$10$K5t0CK89OD8Xj2/S4KQ8Pub6/ChNmh5C7B9ehxOOQ.jUUOL1UqkPy", 1);
--- sql/total-storage-available.sql
+++
@@ -1,3 +0,0 @@
-use `LXD_Manager`;
-
-ALTER TABLE `Fleet_Analytics` ADD COLUMN `FA_Total_Storage_Available` BIGINT NOT NULL;
