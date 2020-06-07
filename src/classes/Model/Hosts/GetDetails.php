a5d0aa24a8446aa4e379c46251b987df4830d19e
turtle0x1 <dhope0000@gmail.com>
2020-06-07 20:04:29 +0100

    Replace usage of hostId with Host Object for get host overview

--- src/classes/Model/Hosts/GetDetails.php
+++ src/classes/Model/Hosts/GetDetails.php
@@ -14,23 +14,6 @@ class GetDetails
         $this->container = $container;
     }
 
-    public function getIpAndAlias(int $hostId)
-    {
-        $sql = "SELECT
-                    `Host_Url_And_Port` as `urlAndPort`,
-                    COALESCE(`Host_Alias`, `Host_Url_And_Port`) as `alias`
-                FROM
-                    `Hosts`
-                WHERE
-                    `Host_ID` = :hostId
-                ";
-        $do = $this->database->prepare($sql);
-        $do->execute([
-            ":hostId"=>$hostId
-        ]);
-        return $do->fetch(\PDO::FETCH_ASSOC);
-    }
-
     public function fetchAlias(int $hostId)
     {
         $sql = "SELECT
