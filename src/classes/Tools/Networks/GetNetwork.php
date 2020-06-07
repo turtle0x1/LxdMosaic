6b769dd92e624e5611f56082c5977163024372fd
turtle0x1 <dhope0000@gmail.com>
2020-06-07 19:44:00 +0100

    Remove uneed call to getDetails in GetNetwork

--- src/classes/Tools/Networks/GetNetwork.php
+++ src/classes/Tools/Networks/GetNetwork.php
@@ -2,25 +2,21 @@
 
 namespace dhope0000\LXDClient\Tools\Networks;
 
-use dhope0000\LXDClient\Model\Hosts\GetDetails;
 use dhope0000\LXDClient\Tools\Utilities\StringTools;
 use dhope0000\LXDClient\Objects\Host;
 
 class GetNetwork
 {
-    public function __construct(GetDetails $getDetails)
-    {
-        $this->getDetails = $getDetails;
-    }
-
     public function get(Host $host, string $network)
     {
         $network = $host->networks->info($network);
+
         $network["used_by"] = StringTools::usedByStringsToLinks(
             $host->getHostId(),
             $network["used_by"],
-            $this->getDetails->fetchAlias($host->getHostId())
+            $host->getAlias()
         );
+        
         return $network;
     }
 }
