7c650f4a58776fd55ec7c2a1c1652381e3c31dc2
turtle0x1 <dhope0000@gmail.com>
2020-06-07 03:31:01 +0100

    Replace usage of hostId with Host Object for instance state

--- src/classes/Controllers/Instances/StateController.php
+++ src/classes/Controllers/Instances/StateController.php
@@ -3,6 +3,7 @@ namespace dhope0000\LXDClient\Controllers\Instances;
 
 use dhope0000\LXDClient\Tools\Instances\State;
 use dhope0000\LXDClient\Constants\StateConstants as StateCon;
+use dhope0000\LXDClient\Objects\Host;
 
 class StateController implements \dhope0000\LXDClient\Interfaces\RecordAction
 {
@@ -11,31 +12,31 @@ class StateController implements \dhope0000\LXDClient\Interfaces\RecordAction
         $this->state = $state;
     }
 
-    public function start($hostId, $container, string $alias = null)
+    public function start(Host $host, $container, string $alias = null)
     {
-        $this->state->change($hostId, $container, StateCon::START);
+        $this->state->change($host, $container, StateCon::START);
         return ["state"=>"success", "message"=>"Starting $alias/$container", "code"=>101];
     }
 
-    public function stop($hostId, $container, string $alias = null)
+    public function stop(Host $host, $container, string $alias = null)
     {
-        $this->state->change($hostId, $container, StateCon::STOP);
+        $this->state->change($host, $container, StateCon::STOP);
         return ["state"=>"success", "message"=>"Stopping $alias/$container", "code"=>102];
     }
 
-    public function restart($hostId, $container, string $alias = null)
+    public function restart(Host $host, $container, string $alias = null)
     {
-        $this->state->change($hostId, $container, StateCon::RESTART);
+        $this->state->change($host, $container, StateCon::RESTART);
         return ["state"=>"success", "message"=>"Restarting $alias/$container", "code"=>101];
     }
-    public function freeze($hostId, $container, string $alias = null)
+    public function freeze(Host $host, $container, string $alias = null)
     {
-        $this->state->change($hostId, $container, StateCon::FREEZE);
+        $this->state->change($host, $container, StateCon::FREEZE);
         return ["state"=>"success", "message"=>"Freezing $alias/$container", "code"=>110];
     }
-    public function unfreeze($hostId, $container, string $alias = null)
+    public function unfreeze(Host $host, $container, string $alias = null)
     {
-        $this->state->change($hostId, $container, StateCon::UNFREEZE);
+        $this->state->change($host, $container, StateCon::UNFREEZE);
         return ["state"=>"success", "message"=>"Unfreezing $alias/$container", "code"=>101];
     }
 }
