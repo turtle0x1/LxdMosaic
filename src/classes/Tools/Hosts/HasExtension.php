ea04359c9867f3fcb889f261da3d3a5d6e794e37
turtle0x1 <dhope0000@gmail.com>
2020-06-10 20:53:24 +0100

    Remove unused method from host has extension

--- src/classes/Tools/Hosts/HasExtension.php
+++ src/classes/Tools/Hosts/HasExtension.php
@@ -9,14 +9,6 @@ class HasExtension
 {
     private $hostCache = [];
 
-    public function checkWithClient(Client $client, $extension)
-    {
-        $hostUrl = $client->getUrl();
-        $info = isset($this->hostCache[$hostUrl]) ? $this->hostCache[$hostUrl] : $client->host->info();
-        $this->hostCache[$hostUrl] = $info;
-        return in_array($extension, $info["api_extensions"]);
-    }
-
     public function checkWithHost(Host $host, $extension)
     {
         $hostUrl = $host->getUrl();
