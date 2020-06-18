f29397fdb33e075a8b96795563abb4387043aad7
turtle0x1 <dhope0000@gmail.com>
2020-06-19 00:14:30 +0100

    Fix wrong const value for lxd recursion level

--- src/classes/Constants/LxdRecursionLevels.php
+++ src/classes/Constants/LxdRecursionLevels.php
@@ -5,5 +5,5 @@ class LxdRecursionLevels
 {
     public const INSTANCE_FULL_RECURSION = 2;
     public const INSTANCE_HALF_RECURSION = 1;
-    public const INSTANCE_NO_RECURSION = 1;
+    public const INSTANCE_NO_RECURSION = 0;
 }
