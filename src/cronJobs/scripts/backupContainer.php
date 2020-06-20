6fc17cfc0c2bc811d310c01b7f408f106f5088f4
turtle0x1 <dhope0000@gmail.com>
2020-06-20 14:47:37 +0100

    Added task that builds backup cron jobs from stored schedule

---
+++ src/cronJobs/scripts/backupContainer.php
@@ -0,0 +1,44 @@
+<?php
+
+require __DIR__ . "/../../../vendor/autoload.php";
+
+use dhope0000\LXDClient\Constants\Constants;
+
+$builder = new \DI\ContainerBuilder();
+$container = $builder->build();
+
+$env = new Dotenv\Dotenv(__DIR__ . "/../../../");
+$env->load();
+
+if (count($argv) !== 5) {
+    throw new \Exception("script should be called backupContainer.php hostId instance project strategyId", 1);
+}
+
+$hostId = $argv[1];
+
+if (!is_numeric($hostId)) {
+    throw new \Exception("host must be numeric id", 1);
+}
+
+$instance = $argv[2];
+$project = $argv[3];
+$strategy = $argv[4];
+
+if (!is_numeric($strategy)) {
+    throw new \Exception("Please provide strategy as numeric id", 1);
+}
+
+$getHost = $container->make("dhope0000\LXDClient\Model\Hosts\GetDetails");
+$backupInstance = $container->make("dhope0000\LXDClient\Tools\Instances\Backups\BackupInstance");
+
+$host = $getHost->fetchHost($hostId);
+
+$importAndDelete = $strategy == 2;
+
+$backupInstance->create(
+    $host,
+    $instance,
+    (new \DateTime())->format("Y-m-d H:i:s"),
+    true,
+    $importAndDelete
+);
