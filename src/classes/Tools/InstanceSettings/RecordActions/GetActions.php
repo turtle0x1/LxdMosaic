8078dfa04f46b819b81c6d205255befaf4a24443
turtle0x1 <dhope0000@gmail.com>
2020-06-22 17:36:14 +0100

    Added RouteToName mapping object & script to generate it

--- src/classes/Tools/InstanceSettings/RecordActions/GetActions.php
+++ src/classes/Tools/InstanceSettings/RecordActions/GetActions.php
@@ -4,22 +4,31 @@ namespace dhope0000\LXDClient\Tools\InstanceSettings\RecordActions;
 
 use dhope0000\LXDClient\Tools\User\ValidatePermissions;
 use dhope0000\LXDClient\Model\InstanceSettings\RecordActions\FetchRecordedActions;
+use dhope0000\LXDClient\Objects\RouteToNameMapping;
 
 class GetActions
 {
     private $validatePermissions;
     private $fetchRecordedActions;
 
-    public function __construct(ValidatePermissions $validatePermissions, FetchRecordedActions $fetchRecordedActions)
-    {
+    public function __construct(
+        ValidatePermissions $validatePermissions,
+        FetchRecordedActions $fetchRecordedActions,
+        RouteToNameMapping $routeToNameMapping
+    ) {
         $this->validatePermissions = $validatePermissions;
         $this->fetchRecordedActions = $fetchRecordedActions;
+        $this->routeToNameMapping = $routeToNameMapping;
     }
 
     public function get(int $userId, int $ammount)
     {
         $this->validatePermissions->isAdminOrThrow($userId);
 
-        return $this->fetchRecordedActions->fetch($ammount);
+        $logs = $this->fetchRecordedActions->fetch($ammount);
+        foreach ($logs as &$log) {
+            $log["controllerName"] = $this->routeToNameMapping->getControllerName($log["controller"]);
+        }
+        return $logs;
     }
 }
