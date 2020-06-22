d1cdcc889d7db2c0e6923c499fde487180e1141e
turtle0x1 <dhope0000@gmail.com>
2020-06-22 17:05:28 +0100

    Added symfony route annotation to recorded backups routes

--- src/classes/Controllers/Backups/RestoreBackupController.php
+++ src/classes/Controllers/Backups/RestoreBackupController.php
@@ -4,6 +4,7 @@ namespace dhope0000\LXDClient\Controllers\Backups;
 
 use dhope0000\LXDClient\Tools\Backups\RestoreBackup;
 use dhope0000\LXDClient\Objects\Host;
+use Symfony\Component\Routing\Annotation\Route;
 
 class RestoreBackupController implements \dhope0000\LXDClient\Interfaces\RecordAction
 {
@@ -13,7 +14,9 @@ class RestoreBackupController implements \dhope0000\LXDClient\Interfaces\RecordA
     {
         $this->restoreBackup = $restoreBackup;
     }
-
+    /**
+     * @Route("", name="Restore Local Backup To Host")
+     */
     public function restore(int $backupId, Host $targetHost)
     {
         $response = $this->restoreBackup->restore($backupId, $targetHost);
