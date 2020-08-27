40a5d30617eba253c1f49ef12bc498b4cd164144
turtle0x1 <dhope0000@gmail.com>
2020-08-27 18:07:14 +0100

    HostsCollection implmenets JsonSerializable so it can be logged properly

--- src/classes/Objects/HostsCollection.php
+++ src/classes/Objects/HostsCollection.php
@@ -4,7 +4,7 @@ namespace dhope0000\LXDClient\Objects;
 
 use dhope0000\LXDClient\Objects\Host;
 
-class HostsCollection implements \Iterator
+class HostsCollection implements \Iterator, \JsonSerializable
 {
     private $hosts = [];
     private $index = 0;
@@ -14,6 +14,11 @@ class HostsCollection implements \Iterator
         foreach ($hosts as $host) {
             $this->addHost($host);
         }
+    }
+
+    final public function jsonSerialize()
+    {
+        return $this->hosts;
     }
 
     public function addHost(Host $host)
