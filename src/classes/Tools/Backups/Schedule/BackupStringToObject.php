f5247996dec727886011c24f923c47790038c902
turtle0x1 <dhope0000@gmail.com>
2020-09-16 00:46:03 +0100

    Start work on a internal "language" for backup schedules (#241)
    Because the schedule is stored as a string we have to create some
    "abritary language" to schedule backups
    
    The "language" is
    
        range~ [times]~ [daysOfWeek]~ dayOfMonth~
    
    Using the a tidle (~) as a "end of statment"
    
    This will allow for schedules like
    
        daily~ [10:00]~
        daily~ [06:00, 20:00]~
    
        weekly~ [10:00]~ [0, 2, 4, 6]
        weekly~ [10:00, 13:00]~ [1, 3, 5]
    
        monthly~ [10:00]~ []~ 22~
    
    Then when crunz runs we generate the backup tasks

---
+++ src/classes/Tools/Backups/Schedule/BackupStringToObject.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace dhope0000\LXDClient\Tools\Backups\Schedule;
+
+use dhope0000\LXDClient\Objects\Backups\BackupSchedule;
+
+class BackupStringToObject
+{
+    public function trimString($data)
+    {
+        return trim($data);
+    }
+
+    public function convertStringArrayToPhp($string)
+    {
+        $string = str_replace("[", "", $string);
+        $string = str_replace("]", "", $string);
+        return array_map([$this, "trimString"], explode(",", $string));
+    }
+
+
+    public function parseString($string) :BackupSchedule
+    {
+        $this->validateString($string);
+        
+        $parts = array_map([$this, "trimString"], explode("~", $string));
+
+        $time = $parts[1];
+
+        if (strpos($time, "[") !== false) {
+            $time = $this->convertStringArrayToPhp($time);
+        } else {
+            $time = [$time];
+        }
+
+        $daysOfWeek = [];
+
+        if ($parts[2] !== "[]" && !empty($parts[2])) {
+            $daysOfWeek = $this->convertStringArrayToPhp($parts[2]);
+        }
+
+        return new BackupSchedule(
+            $parts[0],
+            $time,
+            $daysOfWeek,
+            $parts[3] ?? null
+        );
+    }
+
+    public function validateString(string $string) :void
+    {
+        if (substr_count($string, "~") !== 4) {
+            throw new \Exception("Syntax Error: There must be 4 tidles in the string", 1);
+        }
+    }
+}
