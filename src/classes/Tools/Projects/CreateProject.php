cf96bf1c34b859bcda93feed1c33ff3cd7aca4fc
turtle0x1 <dhope0000@gmail.com>
2020-09-21 20:27:40 +0100

    Unset networks feature if host doesn't support it on project creation

--- src/classes/Tools/Projects/CreateProject.php
+++ src/classes/Tools/Projects/CreateProject.php
@@ -3,9 +3,15 @@
 namespace dhope0000\LXDClient\Tools\Projects;
 
 use dhope0000\LXDClient\Objects\HostsCollection;
+use dhope0000\LXDClient\Tools\Hosts\HasExtension;
 
 class CreateProject
 {
+    public function __construct(HasExtension $hasExtension)
+    {
+        $this->hasExtension = $hasExtension;
+    }
+
     public function create(
         HostsCollection $hosts,
         string $name,
@@ -14,6 +20,11 @@ class CreateProject
     ) {
         foreach ($hosts as $host) {
             //TODO Check if project exsits
+            $supportsNetworks = $this->hasExtension->checkWithHost($host, "projects_networks");
+
+            if (!$supportsNetworks) {
+                unset($config["features.networks"]);
+            }
             $host->projects->create($name, $description, $config);
         }
         return true;
