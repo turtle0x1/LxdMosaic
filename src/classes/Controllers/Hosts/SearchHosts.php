69b406de80e73999908f71882ac7b280b5e9f161
turtle0x1 <dhope0000@gmail.com>
2020-09-25 23:34:42 +0100

    Added optional extensions filter to search hosts api

--- src/classes/Controllers/Hosts/SearchHosts.php
+++ src/classes/Controllers/Hosts/SearchHosts.php
@@ -2,20 +2,34 @@
 namespace dhope0000\LXDClient\Controllers\Hosts;
 
 use dhope0000\LXDClient\Model\Hosts\HostList;
+use dhope0000\LXDClient\Tools\Hosts\HasExtension;
 
 class SearchHosts
 {
-    public function __construct(HostList $hostList)
+    public function __construct(HostList $hostList, HasExtension $hasExtension)
     {
         $this->hostList = $hostList;
+        $this->hasExtension = $hasExtension;
     }
 
-    public function search(string $hostSearch)
+    public function search(string $hostSearch, array $extensionRequirements = [])
     {
         $servers = $this->hostList->getOnlineHostsWithDetails();
         $output = [];
         foreach ($servers as $server) {
             if (stripos($server->getAlias(), $hostSearch) !== false) {
+                $doesntHaveExt = false;
+                if (!empty($extensionRequirements)) {
+                    foreach ($extensionRequirements as $requirement) {
+                        if (!$this->hasExtension->checkWithHost($server, $requirement)) {
+                            $doesntHaveExt = true;
+                            break;
+                        }
+                    }
+                }
+                if ($doesntHaveExt) {
+                    continue;
+                }
                 $output[] = [
                     "host"=>$server->getAlias(),
                     "hostId"=>$server->getHostId()
