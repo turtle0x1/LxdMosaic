f6eb37eecb038fc5864c3e85bb6f6be8a2e1bc17
turtle0x1 <dhope0000@gmail.com>
2020-10-03 20:56:49 +0100

    Fix deleting last project when on last project blocks UI (close #261)

--- src/classes/Model/Users/Projects/InsertUserProject.php
+++ src/classes/Model/Users/Projects/InsertUserProject.php
@@ -11,6 +11,26 @@ class InsertUserProject
         $this->database = $database->dbObject;
     }
 
+    public function putUsersOnProjectToProject(int $hostId, string $oldProject, string $newProject)
+    {
+        $sql = "UPDATE
+                    `User_Host_Projects`
+                SET
+                    `UHP_Project` = :newProject
+                WHERE
+                    `UHP_Host_ID` = :hostId
+                AND
+                    `UHP_Project` = :oldProject
+                ";
+        $do = $this->database->prepare($sql);
+        $do->execute([
+            ":newProject"=>$newProject,
+            ":hostId"=>$hostId,
+            ":oldProject"=>$oldProject
+        ]);
+        return $do->rowCount() ? true : false;
+    }
+
     public function insert(int $userId, int $hostId, string $project)
     {
         $sql = "INSERT INTO `User_Host_Projects`(
--- src/classes/Tools/Projects/DeleteProject.php
+++ src/classes/Tools/Projects/DeleteProject.php
@@ -3,11 +3,21 @@
 namespace dhope0000\LXDClient\Tools\Projects;
 
 use dhope0000\LXDClient\Objects\Host;
+use dhope0000\LXDClient\Model\Users\Projects\InsertUserProject;
 
 class DeleteProject
 {
+    public function __construct(InsertUserProject $insertUserProject)
+    {
+        $this->insertUserProject = $insertUserProject;
+    }
+
     public function remove(Host $host, string $project)
     {
+        $hostProjects = $host->projects->all();
+
+        $this->insertUserProject->putUsersOnProjectToProject($host->getHostId(), $project, "default");
+
         return $host->projects->remove($project);
     }
 }
