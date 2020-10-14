169aa3bc12406638378f5ddb7b953909cd370472
turtle0x1 <dhope0000@gmail.com>
2020-10-14 22:27:27 +0100

    Switch to the user universe for getting storage
    LXD choices dont make sense to me - custom volumes per project, but they
    cant be used as root disks(?) - so they still need access to global
    storage...

---
+++ src/classes/Tools/Storage/GetUserStorage.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace dhope0000\LXDClient\Tools\Storage;
+
+use dhope0000\LXDClient\Tools\Universe;
+use dhope0000\LXDClient\Objects\Host;
+
+class GetUserStorage
+{
+    public function __construct(
+        Universe $universe
+    ) {
+        $this->universe = $universe;
+    }
+
+    public function getAll($userId)
+    {
+        $clusters = $this->universe->getEntitiesUserHasAccesTo($userId, "volumes");
+
+        $stats = [
+            "storage"=>[
+                "total"=>0,
+                "used"=>0
+            ],
+            "inodes"=>[
+                "total"=>0,
+                "used"=>0
+            ],
+        ];
+
+        foreach ($clusters["clusters"] as $clusterIndex => $cluster) {
+            foreach ($cluster["members"] as $hostIndex => &$host) {
+                $pools = $this->getHostPools($host);
+                $stats = $this->calculateStats($stats, $pools);
+                $host->setCustomProp("pools", $pools);
+            }
+        }
+
+        foreach ($clusters["standalone"]["members"] as $host) {
+            $pools = $this->getHostPools($host);
+            $stats = $this->calculateStats($stats, $pools);
+            $host->setCustomProp("pools", $pools);
+        }
+
+        return [
+            "hostDetails"=>$clusters,
+            "stats"=>$stats
+        ];
+    }
+
+    private function calculateStats($stats, $pools)
+    {
+        foreach ($pools as $pool) {
+            $stats["storage"]["total"] += $pool["resources"]["space"]["total"];
+            $stats["storage"]["used"] += $pool["resources"]["space"]["used"];
+
+            $stats["inodes"]["total"] += $pool["resources"]["inodes"]["total"];
+            $stats["inodes"]["used"] += $pool["resources"]["inodes"]["used"];
+        }
+        return $stats;
+    }
+
+    private function getHostPools(Host $host)
+    {
+        if (!$host->hostOnline()) {
+            return [];
+        }
+
+        //TODO Recursion
+        $pools = $host->storage->all();
+        $withResources = [];
+        foreach ($pools as $pool) {
+            $withResources[] = [
+                "name"=>$pool,
+                "resources"=>$host->storage->resources->info($pool)
+            ];
+        }
+
+        return $withResources;
+    }
+}
