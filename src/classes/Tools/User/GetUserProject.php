f3b8a01600a72581fd861499204d2be8ba7b23a2
turtle0x1 <dhope0000@gmail.com>
2020-10-21 22:02:37 +0100

    Give admin access to default project if they haven't been give explict access to specific projects

--- src/classes/Tools/User/GetUserProject.php
+++ src/classes/Tools/User/GetUserProject.php
@@ -5,29 +5,40 @@ namespace dhope0000\LXDClient\Tools\User;
 use dhope0000\LXDClient\Model\Users\AllowedProjects\FetchAllowedProjects;
 use dhope0000\LXDClient\Model\Users\Projects\FetchUserProject;
 use dhope0000\LXDClient\Tools\User\SetUserProject;
+use dhope0000\LXDClient\Model\Users\FetchUserDetails;
 
 class GetUserProject
 {
     public function __construct(
         FetchAllowedProjects $fetchAllowedProjects,
         FetchUserProject $fetchUserProject,
-        SetUserProject $setUserProject
+        SetUserProject $setUserProject,
+        FetchUserDetails $fetchUserDetails
     ) {
         $this->fetchAllowedProjects = $fetchAllowedProjects;
         $this->fetchUserProject = $fetchUserProject;
         $this->setUserProject = $setUserProject;
+        $this->fetchUserDetails = $fetchUserDetails;
     }
 
     public function getForHost(int $userId, $host) :string
     {
         $project = $this->fetchUserProject->fetch($userId, $host->getHostId());
+
         if (!empty($project)) {
             return $project;
+        }
+        
+        $isAdmin = $this->fetchUserDetails->isAdmin($userId);
+
+        if ($isAdmin) {
+            $this->setUserProject->set($userId, $host->getHostId(), $project);
+            return "default";
         }
 
         $allowedProjects = $this->fetchAllowedProjects->fetchForHost($userId, $host->getHostId());
 
-        if(empty($allowedProjects)){
+        if (empty($allowedProjects)) {
             throw new \Exception("Trying to access host with no allowed projects", 1);
         }
 
