35eee6ca6a1425439ec9bd843a7c117427b67427
turtle0x1 <dhope0000@gmail.com>
2020-10-21 22:08:54 +0100

    Allow admin to set themselves to any project

--- src/classes/Tools/User/SetUserProject.php
+++ src/classes/Tools/User/SetUserProject.php
@@ -4,25 +4,33 @@ namespace dhope0000\LXDClient\Tools\User;
 
 use dhope0000\LXDClient\Model\Users\Projects\InsertUserProject;
 use dhope0000\LXDClient\Model\Users\AllowedProjects\FetchAllowedProjects;
+use dhope0000\LXDClient\Model\Users\FetchUserDetails;
 
 class SetUserProject
 {
-    public function __construct(InsertUserProject $insertUserProject, FetchAllowedProjects $fetchAllowedProjects)
-    {
+    public function __construct(
+        InsertUserProject $insertUserProject,
+        FetchAllowedProjects $fetchAllowedProjects,
+        FetchUserDetails $fetchUserDetails
+    ) {
         $this->insertUserProject = $insertUserProject;
         $this->fetchAllowedProjects = $fetchAllowedProjects;
+        $this->fetchUserDetails = $fetchUserDetails;
     }
 
     public function set(int $userId, int $hostId, string $project)
     {
-        $allowedProjects = $this->fetchAllowedProjects->fetchAll($userId);
+        $isAdmin = $this->fetchUserDetails->isAdmin($userId);
 
-        if (!isset($allowedProjects[$hostId])) {
-            throw new \Exception("No access to this host", 1);
-        }
+        if (!$isAdmin) {
+            $allowedProjects = $this->fetchAllowedProjects->fetchAll($userId);
+            if (!isset($allowedProjects[$hostId])) {
+                throw new \Exception("No access to this host", 1);
+            }
 
-        if (!in_array($project, $allowedProjects[$hostId])) {
-            throw new \Exception("No access to this project", 1);
+            if (!in_array($project, $allowedProjects[$hostId])) {
+                throw new \Exception("No access to this project", 1);
+            }
         }
 
         $this->insertUserProject->insert($userId, $hostId, $project);
