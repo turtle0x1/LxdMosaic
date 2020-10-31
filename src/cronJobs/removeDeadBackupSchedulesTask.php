77653e5046548414a1f71103973919b807990612
turtle0x1 <dhope0000@gmail.com>
2020-10-31 17:29:20 +0000

    Add cron job to disable backup jobs for offline hosts & deleted instance

---
+++ src/cronJobs/removeDeadBackupSchedulesTask.php
@@ -0,0 +1,11 @@
+<?php
+
+use Crunz\Schedule;
+
+$schedule = new Schedule();
+$task = $schedule->run(PHP_BINARY . '  ' . __DIR__ . '/scripts/removeDeadBackupSchedules.php');
+$task
+    ->everyFiveMinutes()
+    ->description('Check for instances that may have been deleted but schedules remain');
+
+return $schedule;
