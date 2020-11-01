37dc95eeb923877c6d0d57b3c65cabeb9a9b5b66
turtle0x1 <dhope0000@gmail.com>
2020-11-01 18:42:57 +0000

    Check universe limits user access

--- tests/test/Tools/UniverseTest.php
+++ tests/test/Tools/UniverseTest.php
@@ -27,4 +27,25 @@ final class UniverseTest extends TestCase
         $this->expectException(\Exception::class);
         $result = $this->universe->getEntitiesUserHasAccesTo(999, null);
     }
+
+    /**
+     * checking admin can see the new entity (profiles) inline with permisions.
+     * (the fact its "profiles" is irrelevant)
+     */
+    public function test_admin_can_see_new_entity()
+    {
+        $r = $this->universe->getEntitiesUserHasAccesTo(1, "profiles");
+        $result = $r["standalone"]["members"][0]->getCustomProp("profiles");
+
+        $this->assertEquals(["default", "testProfile"], $result);
+    }
+    /**
+     * checking "test" cant see the new entity (profiles) inline with permisions
+     */
+    public function test_user_two_cant_see_new_entity()
+    {
+        $r = $this->universe->getEntitiesUserHasAccesTo(2, "profiles");
+        $result = $r["standalone"]["members"]->getAllHosts()[0]->getCustomProp("profiles");
+        $this->assertEquals(["default"], $result);
+    }
 }
