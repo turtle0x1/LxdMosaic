7764642c18c41783d6c40e6dc293fcc810c54d3d
turtle0x1 <dhope0000@gmail.com>
2020-11-29 02:42:51 +0000

    Filter networks used_by based on user

--- src/classes/Tools/Networks/GetNetwork.php
+++ src/classes/Tools/Networks/GetNetwork.php
@@ -2,16 +2,25 @@
 
 namespace dhope0000\LXDClient\Tools\Networks;
 
+use dhope0000\LXDClient\Tools\Utilities\UsedByFilter;
 use dhope0000\LXDClient\Tools\Utilities\StringTools;
 use dhope0000\LXDClient\Objects\Host;
 
 class GetNetwork
 {
-    public function get(Host $host, string $network)
+    private $usedByFilter;
+
+    public function __construct(UsedByFilter $usedByFilter)
+    {
+        $this->usedByFilter = $usedByFilter;
+    }
+
+    public function get(int $userId, Host $host, string $network)
     {
         $network = $host->networks->info($network);
 
         if ($network["used_by"] !== null) {
+            $network["used_by"] = $this->usedByFilter->filterUserProjects($userId, $host, $network["used_by"]);
             $network["used_by"] = StringTools::usedByStringsToLinks(
                 $host,
                 $network["used_by"]
