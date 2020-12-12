f2c77e2b05fc03fd65ba3b44c21b541385d42ebc
turtle0x1 <dhope0000@gmail.com>
2020-12-12 21:20:27 +0000

    Add test for get user allowed projects

---
+++ tests/test/Controllers/User/AllowedProjects/GetUserAllowedProjectsControllerTest.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class GetUserAllowedProjectsControllerTest extends TestCase
+{
+    public function setUp() :void
+    {
+        $builder = new \DI\ContainerBuilder();
+        $builder->useAnnotations(true);
+        $container = $builder->build();
+        $this->routeApi = $container->make("dhope0000\LXDClient\App\RouteApi");
+    }
+
+    public function test_nonAdminTryingToGetUserAllowedProjects() :void
+    {
+        $this->expectException(\Exception::class);
+
+        $_POST = ["targetUser"=>1];
+
+        $this->routeApi->route(
+            array_filter(explode('/', '/User/AllowedProjects/GetUserAllowedProjectsController/get')),
+            ["userid"=>2],
+            true
+        );
+    }
+
+    public function test_adminGettingAdminAllowedProjects() :void
+    {
+        $_POST = ["targetUser"=>1];
+
+        $result = $this->routeApi->route(
+            array_filter(explode('/', '/User/AllowedProjects/GetUserAllowedProjectsController/get')),
+            ["userid"=>1],
+            true
+        );
+
+        $this->assertEquals(["isAdmin"=>true, "projects"=>[]], $result);
+    }
+
+    public function test_adminGettingNonAdminAllowedProjects() :void
+    {
+        $_POST = ["targetUser"=>2];
+
+        $result = $this->routeApi->route(
+            array_filter(explode('/', '/User/AllowedProjects/GetUserAllowedProjectsController/get')),
+            ["userid"=>1],
+            true
+        );
+
+        $this->assertTrue($result["isAdmin"] == false);
+        $this->assertTrue(count($result["projects"]) > 0);
+        $this->assertEquals($result["projects"][0]->getCustomProp("projects"), [
+            "testProject"
+        ]);
+    }
+}
