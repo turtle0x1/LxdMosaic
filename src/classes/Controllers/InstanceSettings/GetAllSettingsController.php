e389ec2826b6974ee13bb0efc21cf14cef488346
turtle0x1 <dhope0000@gmail.com>
2020-12-13 00:21:00 +0000

    Validate user is admin before getting all settings on the api

--- src/classes/Controllers/InstanceSettings/GetAllSettingsController.php
+++ src/classes/Controllers/InstanceSettings/GetAllSettingsController.php
@@ -3,16 +3,20 @@
 namespace dhope0000\LXDClient\Controllers\InstanceSettings;
 
 use dhope0000\LXDClient\Model\InstanceSettings\GetSettings;
+use dhope0000\LXDClient\Tools\User\ValidatePermissions;
 
 class GetAllSettingsController
 {
-    public function __construct(GetSettings $getSettings)
+    public function __construct(GetSettings $getSettings, ValidatePermissions $validatePermissions)
     {
         $this->getSettings = $getSettings;
+        $this->validatePermissions = $validatePermissions;
     }
 
-    public function getAll()
+    public function getAll(int $userId)
     {
+        $this->validatePermissions->isAdminOrThrow($userId);
+
         return $this->getSettings->getAllSettingsWithLatestValues();
     }
 }
