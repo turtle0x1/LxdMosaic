1aa915405fcca77791f543cd64a5ee844688d3be
turtle0x1 <dhope0000@gmail.com>
2021-01-17 16:24:42 +0000

    Update changelog & install files for release

--- examples/install_with_clone.sh
+++ examples/install_with_clone.sh
@@ -84,7 +84,7 @@ fi
 # Move in LxdManager
 cd /var/www/LxdMosaic || exit
 
-git checkout v0.11.3
+git checkout v0.11.4
 
 npm install || exit $?
 
--- examples/install_with_clone_centos7.sh
+++ examples/install_with_clone_centos7.sh
@@ -60,7 +60,7 @@ fi
 # Move in LxdManager
 cd /var/www/LxdMosaic || exit
 
-git checkout v0.11.3
+git checkout v0.11.4
 
 npm install
 
