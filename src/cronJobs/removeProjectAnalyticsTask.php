66921449957ca1894fd7d37c3e3eb6a5d168b412
turtle0x1 <dhope0000@gmail.com>
2021-02-04 20:44:16 +0000

    Remove old project metrics every 6 minutes to help keep database lean

--- src/cronJobs/removeProjectAnalyticsTask.php
+++ src/cronJobs/removeProjectAnalyticsTask.php
@@ -5,8 +5,7 @@ use Crunz\Schedule;
 $schedule = new Schedule();
 $task = $schedule->run(PHP_BINARY . '  ' . __DIR__ . '/scripts/removeProjectAnalytics.php');
 $task
-    ->daily()
-    ->at('13:00')
+    ->everySixMinutes()
     ->description('Removing project analytics');
 
 return $schedule;
