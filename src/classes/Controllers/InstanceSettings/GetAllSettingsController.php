c911fe111eadf9cf8d2a95e3644f3fc2f584eb45
turtle0x1 <dhope0000@gmail.com>
2021-05-01 16:03:42 +0100

    Return list of server timezones when loading instance settings

--- src/classes/Controllers/InstanceSettings/GetAllSettingsController.php
+++ src/classes/Controllers/InstanceSettings/GetAllSettingsController.php
@@ -5,13 +5,18 @@ namespace dhope0000\LXDClient\Controllers\InstanceSettings;
 use dhope0000\LXDClient\Model\InstanceSettings\GetSettings;
 use dhope0000\LXDClient\Tools\User\ValidatePermissions;
 use dhope0000\LXDClient\Tools\Utilities\IsUpToDate;
+use dhope0000\LXDClient\Tools\Utilities\DateTools;
 
 class GetAllSettingsController
 {
-    public function __construct(GetSettings $getSettings, ValidatePermissions $validatePermissions)
-    {
+    public function __construct(
+        GetSettings $getSettings,
+        ValidatePermissions $validatePermissions,
+        DateTools $dateTools
+    ) {
         $this->getSettings = $getSettings;
         $this->validatePermissions = $validatePermissions;
+        $this->dateTools = $dateTools;
     }
 
     public function getAll(int $userId)
@@ -20,7 +25,8 @@ class GetAllSettingsController
 
         return [
             "settings"=>$this->getSettings->getAllSettingsWithLatestValues(),
-            "versionDetails"=>IsUpToDate::isIt()
+            "versionDetails"=>IsUpToDate::isIt(),
+            "timezones"=>$this->dateTools->getTimezoneList()
         ];
     }
 }
