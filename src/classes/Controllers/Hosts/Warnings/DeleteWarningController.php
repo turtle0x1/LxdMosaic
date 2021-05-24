a7b1a82122c159afc7953d8ea9f0d2bf4be534de
turtle0x1 <dhope0000@gmail.com>
2021-05-24 17:27:10 +0100

    Add backend for admin to delete warnings

---
+++ src/classes/Controllers/Hosts/Warnings/DeleteWarningController.php
@@ -0,0 +1,21 @@
+<?php
+namespace dhope0000\LXDClient\Controllers\Hosts\Warnings;
+
+use dhope0000\LXDClient\Objects\Host;
+use dhope0000\LXDClient\Tools\Hosts\Warnings\DeleteWarning;
+
+class DeleteWarningController
+{
+    private $deleteWarning;
+
+    public function __construct(DeleteWarning $deleteWarning)
+    {
+        $this->deleteWarning = $deleteWarning;
+    }
+
+    public function delete(int $userId, Host $host, string $id)
+    {
+        $this->deleteWarning->delete($userId, $host, $id);
+        return ["state"=>"success", "message"=>"Deleted warning", "id"=>$id];
+    }
+}
