3134c99f0ec5ff4980e50982c25aeae1875ace17
turtle0x1 <dhope0000@gmail.com>
2021-06-16 20:20:55 +0100

    Add backend for deleting instance type

---
+++ src/classes/Model/Instances/InstanceTypes/DeleteInstanceType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace dhope0000\LXDClient\Model\Instances\InstanceTypes;
+
+use dhope0000\LXDClient\Model\Database\Database;
+
+class DeleteInstanceType
+{
+    public function __construct(Database $database)
+    {
+        $this->database = $database->dbObject;
+    }
+
+    public function delete(int $typeId)
+    {
+        $sql = "DELETE FROM `Instance_Types` WHERE `IT_ID` = :typeId";
+        $do = $this->database->prepare($sql);
+        $do->execute([
+            ":typeId"=>$typeId
+        ]);
+        return $do->rowCount() ?  true : false;
+    }
+}
