229829de1e508ab4746080419f2a56c1cf32aadf
turtle0x1 <dhope0000@gmail.com>
2021-10-02 19:04:53 +0100

    Fix a bug with creating a directory storage pool

--- src/classes/Tools/Storage/CreateStoragePool.php
+++ src/classes/Tools/Storage/CreateStoragePool.php
@@ -8,6 +8,10 @@ class CreateStoragePool
 {
     public function create(HostsCollection $hosts, string $name, string $driver, array $config)
     {
+        if ($driver === "dir" && isset($config["size"])) {
+            unset($config["size"]);
+        }
+
         foreach ($hosts as $host) {
             $host->storage->create($name, $driver, $config);
         }
