cb96c2bf7ba3d56b0ffe97c40bb0bd545e395b58
turtle0x1 <dhope0000@gmail.com>
2021-10-24 19:05:27 +0100

    Fix a bug with token input focusing when it shouldn't

--- src/assets/token/src/jquery.tokeninput.js
+++ src/assets/token/src/jquery.tokeninput.js
@@ -389,7 +389,7 @@ $.TokenList = function (input, url_or_data, settings) {
     this.clear = function() {
         token_list.children("li").each(function() {
             if ($(this).children("input").length === 0) {
-                delete_token($(this));
+                delete_token($(this), false);
             }
         });
         cache = new $.TokenList.Cache();
@@ -584,7 +584,7 @@ $.TokenList = function (input, url_or_data, settings) {
     }
 
     // Delete a token from the token list
-    function delete_token (token) {
+    function delete_token (token, focusInput = true) {
         // Remove the id from the saved list
         var token_data = $.data(token.get(0), "tokeninput");
         var callback = settings.onDelete;
@@ -597,7 +597,9 @@ $.TokenList = function (input, url_or_data, settings) {
         selected_token = null;
 
         // Show the input box and give it focus again
-        input_box.focus();
+        if(focusInput){
+            input_box.focus();
+        }
 
         // Remove this token from the saved list
         saved_tokens = saved_tokens.slice(0,index).concat(saved_tokens.slice(index+1));
@@ -611,8 +613,10 @@ $.TokenList = function (input, url_or_data, settings) {
         if(settings.tokenLimit !== null) {
             input_box
                 .show()
-                .val("")
-                .focus();
+                .val("");
+            if(focusInput){
+                input_box.focus()
+            }
         }
 
         // Execute the onDelete callback if defined
