c655daf361e6e0a084af5e76a7c51a8106b35d79
turtle0x1 <dhope0000@gmail.com>
2021-10-30 15:14:05 +0100

    Add model to delete deployment containers from DB

---
+++ src/classes/Model/Deployments/Containers/DeleteDeploymentInstances.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace dhope0000\LXDClient\Model\Deployments\Containers;
+
+use dhope0000\LXDClient\Model\Database\Database;
+
+class DeleteDeploymentInstances
+{
+    public function __construct(Database $database)
+    {
+        $this->database = $database->dbObject;
+    }
+
+    public function deleteForHost(int $hostId)
+    {
+        $sql = "DELETE FROM
+                    `Deployment_Containers`
+                WHERE
+                    `DC_Host_ID` = :hostId
+                    ";
+        $do = $this->database->prepare($sql);
+        $do->execute([
+            ":hostId"=>$hostId
+        ]);
+        return $do->rowCount() ? true : false;
+    }
+}
