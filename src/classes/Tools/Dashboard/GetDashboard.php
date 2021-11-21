aba5794d235b2a6f784d567b9a6d7cbba553a48b
turtle0x1 <dhope0000@gmail.com>
2021-11-21 04:05:58 +0000

    Dont return resources when fetching the dashboard

--- src/classes/Tools/Dashboard/GetDashboard.php
+++ src/classes/Tools/Dashboard/GetDashboard.php
@@ -4,7 +4,6 @@ namespace dhope0000\LXDClient\Tools\Dashboard;
 
 use dhope0000\LXDClient\Model\Users\Dashboard\FetchUserDashboards;
 use dhope0000\LXDClient\Tools\Universe;
-use dhope0000\LXDClient\Tools\Hosts\GetResources;
 use dhope0000\LXDClient\Tools\User\GetUserProject;
 use dhope0000\LXDClient\Tools\ProjectAnalytics\GetGraphableProjectAnalytics;
 
@@ -13,21 +12,18 @@ class GetDashboard
     public function __construct(
         FetchUserDashboards $fetchUserDashboards,
         Universe $universe,
-        GetResources $getResources,
         GetUserProject $getUserProject,
         GetGraphableProjectAnalytics $getGraphableProjectAnalytics
     ) {
         $this->fetchUserDashboards = $fetchUserDashboards;
         $this->universe = $universe;
-        $this->getResources = $getResources;
         $this->getUserProject = $getUserProject;
         $this->getGraphableProjectAnalytics = $getGraphableProjectAnalytics;
     }
 
     public function get($userId, string $history = "-30 minutes")
     {
-        $clustersAndHosts = $this->universe->getEntitiesUserHasAccesTo($userId, "projects");
-        $clustersAndHosts = $this->addCurrentProjects($userId, $clustersAndHosts);
+        $clustersAndHosts = $this->universe->getEntitiesUserHasAccesTo($userId);
         $dashboards = $this->fetchUserDashboards->fetchAll($userId);
         $projectGraphData = $this->getGraphableProjectAnalytics->getCurrent($userId, $history);
 
@@ -36,32 +32,5 @@ class GetDashboard
             "clustersAndHosts"=>$clustersAndHosts,
             "projectsUsageGraphData"=>$projectGraphData["totals"]
         ];
-    }
-
-    private function addCurrentProjects($userId, $clustersAndHosts)
-    {
-        foreach ($clustersAndHosts["clusters"] as $index => $cluster) {
-            foreach ($cluster["members"] as $member) {
-                $project = $this->getUserProject->getForHost($userId, $member);
-                $member->setCustomProp("currentProject", $project);
-                $member->setCustomProp("resources", $this->getResources($member));
-            }
-        }
-        foreach ($clustersAndHosts["standalone"]["members"] as $index => $member) {
-            $project = $this->getUserProject->getForHost($userId, $member);
-            $member->setCustomProp("currentProject", $project);
-            $member->setCustomProp("resources", $this->getResources($member));
-        }
-        return $clustersAndHosts;
-    }
-
-    private function getResources($member)
-    {
-        if ($member->hostOnline() == false) {
-            return [];
-        }
-        $r = $this->getResources->getHostExtended($member);
-        unset($r["projects"]);
-        return $r;
     }
 }
