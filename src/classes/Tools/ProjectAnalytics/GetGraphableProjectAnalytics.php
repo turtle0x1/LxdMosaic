d2660b5954239de713f621514686a901b783c08a
turtle0x1 <dhope0000@gmail.com>
2021-11-21 00:18:51 +0000

    Use date ranges instead of created dates for project analytics
    This makes sure when data isn't available the graphs show an empty
    spot instead of "bunching up" the data to make it look like one line.

--- src/classes/Tools/ProjectAnalytics/GetGraphableProjectAnalytics.php
+++ src/classes/Tools/ProjectAnalytics/GetGraphableProjectAnalytics.php
@@ -5,33 +5,99 @@ namespace dhope0000\LXDClient\Tools\ProjectAnalytics;
 use dhope0000\LXDClient\Model\ProjectAnalytics\FetchAnalytics;
 use dhope0000\LXDClient\Model\Users\FetchUserDetails;
 use dhope0000\LXDClient\Model\Users\AllowedProjects\FetchAllowedProjects;
+use dhope0000\LXDClient\Tools\Utilities\DateTools;
 
 class GetGraphableProjectAnalytics
 {
     private $fetchAnalytics;
     private $fetchUserDetails;
     private $fetchAllowedProjects;
+    private $dateTools;
 
     public function __construct(
         FetchAnalytics $fetchAnalytics,
         FetchUserDetails $fetchUserDetails,
-        FetchAllowedProjects $fetchAllowedProjects
+        FetchAllowedProjects $fetchAllowedProjects,
+        DateTools $dateTools
     ) {
         $this->fetchAnalytics = $fetchAnalytics;
         $this->fetchUserDetails = $fetchUserDetails;
         $this->fetchAllowedProjects = $fetchAllowedProjects;
+        $this->dateTools = $dateTools;
     }
 
     public function getCurrent(int $userId, string $history = "-30 minutes")
     {
         $startDate = (new \DateTime())->modify($history);
         $endDate = (new \DateTime());
+        $rangeTemplate = $this->getDateInternval($startDate, $endDate);
+        // Reconstruct the first date to account for rounding done when
+        // setting up the range template
+        $startDate = new \DateTimeImmutable(array_keys($rangeTemplate)[0]);
 
         $enteries = $this->fetchAnalytics->fetchBetween($startDate, $endDate);
-        return $this->groupEnteries($userId, $enteries);
+        return $this->groupEnteries($userId, $enteries, $rangeTemplate);
     }
 
-    private function groupEnteries(int $userId, array $entries)
+    private function getDateInternval($start, $end)
+    {
+        $period = new \DatePeriod(
+            $start,
+            new \DateInterval('P1D'),
+            $end->setTime(23, 59)
+        );
+
+        $numberOfDays = (int) ($period->getStartDate())->diff($period->getEndDate())->format("%R%a");
+        $numberOfMinutes = (int) ($period->getStartDate())->diff($period->getEndDate())->i;
+        $numberOfHours = (int) ($period->getStartDate())->diff($period->getEndDate())->h;
+
+        $step = 5;
+        if ($numberOfDays == 0 && $numberOfHours >= 3) {
+            $step = 15;
+        } elseif ($numberOfDays == 0 && $numberOfHours >= 6) {
+            $step = 30;
+        }
+
+        $dataByDate = [];
+
+        foreach ($period as $key => $value) {
+            $dateString = $value->format("Y-m-d");
+
+            if ($numberOfDays >= 1 && $numberOfHours > 12) {
+                if ($numberOfDays <= 7) {
+                    $dataByDate[$dateString . " " . "08:00"] = null;
+                    $dataByDate[$dateString . " " . "12:00"] = null;
+                    $dataByDate[$dateString . " " . "18:00"] = null;
+                } else {
+                    $dataByDate[$dateString. " " . "12:00"] = null;
+                }
+                continue;
+            }
+
+            $startMinute = 0;
+            $startHour = 0;
+            $stopAtNow = $dateString === $period->getEndDate()->format("Y-m-d");
+
+            // If this day is the start date
+            if ($period->getStartDate()->format("Y-m-d") === $dateString) {
+                $startHour = $period->getStartDate()->format("H");
+
+                $startMinute = $period->getStartDate()->format("i");
+
+                // round down to the nearest point
+                $startMinute = floor($startMinute / $step) * $step;
+            }
+
+            $points = DateTools::hoursRange($startHour, 24, $step, "", $stopAtNow, $startMinute);
+
+            foreach ($points as $time=>$value) {
+                $dataByDate[$dateString . " " . $time] = $value;
+            }
+        }
+        return $dataByDate;
+    }
+
+    private function groupEnteries(int $userId, array $entries, $rangeTemplate)
     {
         $isAdmin = (bool) $this->fetchUserDetails->isAdmin($userId);
         $allowedProjects = $this->fetchAllowedProjects->fetchAll($userId);
@@ -62,20 +128,23 @@ class GetGraphableProjectAnalytics
             }
 
             if (!isset($output[$alias][$project][$type])) {
-                $output[$alias][$project][$type] = [];
+                $output[$alias][$project][$type] = $rangeTemplate;
             }
 
             if (!isset($typeTotals[$type])) {
-                $typeTotals[$type] = [];
+                $typeTotals[$type] = $rangeTemplate;
             }
 
-            if (!isset($typeTotals[$type][$entry["created"]])) {
-                $typeTotals[$type][$entry["created"]] = 0;
+            $created = (new \DateTime($entry["created"]))->format("Y-m-d H:i");
+            // Slower than isset() but because the values are null isset()
+            // cant be used
+            if (!array_key_exists($created, $typeTotals[$type])) {
+                continue;
             }
 
-            $typeTotals[$type][$entry["created"]] += $entry["value"];
+            $typeTotals[$type][$created] += $entry["value"];
 
-            $output[$alias][$project][$type][] = [
+            $output[$alias][$project][$type][$created] = [
                 "created"=>$entry["created"],
                 "value"=>$entry["value"],
                 "limit"=>$entry["limit"]
