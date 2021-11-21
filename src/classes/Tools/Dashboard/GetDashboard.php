f8a6aa4057da9b2952084f382248f23697f8a8c9
turtle0x1 <dhope0000@gmail.com>
2021-11-21 04:32:45 +0000

    Dont load clusters and hosts when loading dashboard (not needed)

--- src/classes/Tools/Dashboard/GetDashboard.php
+++ src/classes/Tools/Dashboard/GetDashboard.php
@@ -3,33 +3,25 @@
 namespace dhope0000\LXDClient\Tools\Dashboard;
 
 use dhope0000\LXDClient\Model\Users\Dashboard\FetchUserDashboards;
-use dhope0000\LXDClient\Tools\Universe;
-use dhope0000\LXDClient\Tools\User\GetUserProject;
 use dhope0000\LXDClient\Tools\ProjectAnalytics\GetGraphableProjectAnalytics;
 
 class GetDashboard
 {
     public function __construct(
         FetchUserDashboards $fetchUserDashboards,
-        Universe $universe,
-        GetUserProject $getUserProject,
         GetGraphableProjectAnalytics $getGraphableProjectAnalytics
     ) {
         $this->fetchUserDashboards = $fetchUserDashboards;
-        $this->universe = $universe;
-        $this->getUserProject = $getUserProject;
         $this->getGraphableProjectAnalytics = $getGraphableProjectAnalytics;
     }
 
     public function get($userId, string $history = "-30 minutes")
     {
-        $clustersAndHosts = $this->universe->getEntitiesUserHasAccesTo($userId);
         $dashboards = $this->fetchUserDashboards->fetchAll($userId);
         $projectGraphData = $this->getGraphableProjectAnalytics->getCurrent($userId, $history);
 
         return [
             "userDashboards"=>$dashboards,
-            "clustersAndHosts"=>$clustersAndHosts,
             "projectsUsageGraphData"=>$projectGraphData["totals"]
         ];
     }
