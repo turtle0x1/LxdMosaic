f4f32f73c58e76b9873446def029e97cd4c9def6
turtle0x1 <dhope0000@gmail.com>
2021-12-02 23:33:11 +0000

    Drop grouped instances & add project analytics from / to GetHostOverview
    This will decrease load times for the view and give us the data to
    create a better dashboard.

--- src/classes/Tools/Hosts/GetHostOverview.php
+++ src/classes/Tools/Hosts/GetHostOverview.php
@@ -8,6 +8,8 @@ use dhope0000\LXDClient\Objects\Host;
 use dhope0000\LXDClient\Tools\Hosts\HasExtension;
 use dhope0000\LXDClient\Constants\LxdApiExtensions;
 use dhope0000\LXDClient\Model\Users\FetchUserDetails;
+use dhope0000\LXDClient\Model\Users\AllowedProjects\FetchAllowedProjects;
+use dhope0000\LXDClient\Tools\ProjectAnalytics\GetGraphableProjectAnalytics;
 
 class GetHostOverview
 {
@@ -16,61 +18,42 @@ class GetHostOverview
         GetResources $getResources,
         HasExtension $hasExtension,
         FetchUserDetails $fetchUserDetails,
-        FetchAllowedProjects $fetchAllowedProjects
+        FetchAllowedProjects $fetchAllowedProjects,
+        GetGraphableProjectAnalytics $getGraphableProjectAnalytics
     ) {
         $this->getHostsInstances = $getHostsInstances;
         $this->getResources = $getResources;
         $this->hasExtension = $hasExtension;
         $this->fetchUserDetails = $fetchUserDetails;
+        $this->fetchAllowedProjects = $fetchAllowedProjects;
+        $this->getGraphableProjectAnalytics = $getGraphableProjectAnalytics;
     }
 
     public function get(int $userId, Host $host)
     {
-        $containers = $this->getHostsInstances->getContainers($host);
-        $sortedContainers = $this->sortContainersByState($containers);
-        $containerStats = $this->calcContainerStats($containers);
         $supportsWarnings = $this->hasExtension->checkWithHost($host, LxdApiExtensions::WARNINGS);
+
         $isAdmin = $this->fetchUserDetails->isAdmin($userId);
+
+        $warnings = [];
+
+        if ($supportsWarnings && $isAdmin) {
+            $warnings = $host->warnings->all();
+        }
+
         $resources = $this->getResources->getHostExtended($host);
 
         if (!$isAdmin) {
             $resources["projects"] = $this->fetchAllowedProjects->fetchForUserHost($userId, $host->getHostId());
         }
+
+        $projectAnalytics = $this->getGraphableProjectAnalytics->getCurrent($userId, "-30 minutes", $host)["totals"];
+
         return [
             "header"=>$host,
-            "containers"=>$sortedContainers,
-            "containerStats"=>$containerStats,
-            "supportsWarnings"=>$supportsWarnings
             "resources"=>$resources,
+            "warnings"=>$warnings,
+            "projectAnalytics"=>$projectAnalytics
         ];
-    }
-
-    private function sortContainersByState(array $containers)
-    {
-        $output = [];
-        foreach ($containers as $containerName => $container) {
-            if (!isset($output[$container["state"]["status"]])) {
-                $output[$container["state"]["status"]] = [];
-            }
-
-            $output[$container["state"]["status"]][$containerName] = $container;
-        }
-        return $output;
-    }
-
-    private function calcContainerStats(array $containers)
-    {
-        $stats = [
-            "online"=>0,
-            "offline"=>0
-        ];
-        foreach ($containers as $container) {
-            if ($container["state"]["status_code"] == 103) {
-                $stats["online"]++;
-            } else {
-                $stats["offline"]++;
-            }
-        }
-        return $stats;
     }
 }
