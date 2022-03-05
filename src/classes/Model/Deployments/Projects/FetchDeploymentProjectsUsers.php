cb6732c073701984f57f2579cf2d942e1b662529
turtle0x1 <dhope0000@gmail.com>
2022-03-05 22:50:48 +0000

    Add backend to authorise user against deployment

---
+++ src/classes/Model/Deployments/Projects/FetchDeploymentProjectsUsers.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace dhope0000\LXDClient\Model\Deployments\Projects;
+
+use dhope0000\LXDClient\Model\Database\Database;
+
+class FetchDeploymentProjectsUsers
+{
+    private $database;
+
+    public function __construct(Database $database)
+    {
+        $this->database = $database->dbObject;
+    }
+
+    public function userHasAccess(int $userId, int $deploymentId) :bool
+    {
+        $sql = "SELECT
+                    1
+                FROM
+                    `Users`
+                LEFT JOIN `User_Allowed_Projects` ON
+                    `User_Allowed_Projects`.`UAP_User_ID` = `User_ID`
+                LEFT JOIN `Deployment_Projects` ON
+                    `DP_Deployment_ID` = :deploymentId AND
+                    `DP_Host_ID` = `User_Allowed_Projects`.`UAP_Host_ID` AND
+                    `DP_Project` = `User_Allowed_Projects`.`UAP_Project`
+                WHERE
+                    `User_ID` = :userId
+                AND
+                    (
+                        `Deployment_Projects`.`DP_Project` IS NOT NULL
+                        OR
+                        `User_Admin` = 1
+                    )
+                ";
+        $do = $this->database->prepare($sql);
+        $do->execute([
+            ":deploymentId"=>$deploymentId,
+            ":userId"=>$userId
+        ]);
+        return $do->fetchColumn() ? true : false;
+    }
+}
