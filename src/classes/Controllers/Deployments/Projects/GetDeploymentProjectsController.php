9955145b453d4f24ab4b23732bbdad0f2e517d72
turtle0x1 <dhope0000@gmail.com>
2022-03-05 23:15:04 +0000

    Authorise user when getting deployment projects

--- src/classes/Controllers/Deployments/Projects/GetDeploymentProjectsController.php
+++ src/classes/Controllers/Deployments/Projects/GetDeploymentProjectsController.php
@@ -2,22 +2,26 @@
 
 namespace dhope0000\LXDClient\Controllers\Deployments\Projects;
 
+use dhope0000\LXDClient\Tools\Deployments\Authorise\AuthoriseDeploymentAccess;
 use dhope0000\LXDClient\Model\Deployments\Projects\FetchDeploymentProjects;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GetDeploymentProjectsController
 {
+    private $authoriseDeploymentAccess;
     private $fetchDeploymentProjects;
 
-    public function __construct(FetchDeploymentProjects $fetchDeploymentProjects)
+    public function __construct(AuthoriseDeploymentAccess $authoriseDeploymentAccess, FetchDeploymentProjects $fetchDeploymentProjects)
     {
+        $this->authoriseDeploymentAccess = $authoriseDeploymentAccess;
         $this->fetchDeploymentProjects = $fetchDeploymentProjects;
     }
     /**
      * @Route("", name="Get Deployment Projects")
      */
-    public function get(int $deploymentId)
+    public function get(int $userId, int $deploymentId)
     {
+        $this->authoriseDeploymentAccess->authorise($userId, $deploymentId);
         return $this->fetchDeploymentProjects->fetchAll($deploymentId);
     }
 }
