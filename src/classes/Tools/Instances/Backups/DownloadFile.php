e0c558a7c93783c666ada3c47682d912b0be59db
turtle0x1 <dhope0000@gmail.com>
2022-03-06 19:36:00 +0000

    Update Download backup & hostOnline scripts to use new method signature

--- src/classes/Tools/Instances/Backups/DownloadFile.php
+++ src/classes/Tools/Instances/Backups/DownloadFile.php
@@ -4,21 +4,24 @@ namespace dhope0000\LXDClient\Tools\Instances\Backups;
 
 use dhope0000\LXDClient\Model\Client\LxdClient;
 use GuzzleHttp\Client;
-
 use dhope0000\LXDClient\Objects\Host;
+use dhope0000\LXDClient\Model\Hosts\GetDetails;
 
 class DownloadFile
 {
-    public function __construct(LxdClient $lxdClient)
+    public function __construct(LxdClient $lxdClient, GetDetails $getDetails)
     {
         $this->lxdClient = $lxdClient;
+        $this->getDetails = $getDetails;
     }
 
     public function download(Host $host, string $project, $instance, string $backupFilePath, $backup)
     {
         $certPath = $_ENV["LXD_CERTS_DIR"] .  $host->getCertPath();
 
-        $config = $this->lxdClient->createConfigArray($certPath);
+        $socketPath = $this->getDetails->getSocketPath($host->getHostId());
+
+        $config = $this->lxdClient->createConfigArray($certPath, $socketPath);
 
         $config = array_merge($config, [
           'sink' => $backupFilePath,
