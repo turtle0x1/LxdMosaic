63fff8cec231486beaf771470f438f8700215ef3
turtle0x1 <dhope0000@gmail.com>
2022-05-29 01:55:05 +0100

    Use cron method instead of every* methods in cronjob definitions
    close #503

--- src/cronJobs/removeProjectAnalyticsTask.php
+++ src/cronJobs/removeProjectAnalyticsTask.php
@@ -5,7 +5,7 @@ use Crunz\Schedule;
 $schedule = new Schedule();
 $task = $schedule->run(PHP_BINARY . '  ' . __DIR__ . '/scripts/removeProjectAnalytics.php');
 $task
-    ->everySixMinutes()
+    ->cron("*/6 * * * * ")
     ->description('Removing project analytics');
 
 return $schedule;
