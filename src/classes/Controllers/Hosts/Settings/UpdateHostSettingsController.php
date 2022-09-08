2bd3a3d633e835f486d92c5fdce0f0b46968484a
turtle0x1 <dhope0000@gmail.com>
2022-09-08 21:27:45 +0100

    Add backend to update host settings

---
+++ src/classes/Controllers/Hosts/Settings/UpdateHostSettingsController.php
@@ -0,0 +1,29 @@
+<?php
+namespace dhope0000\LXDClient\Controllers\Hosts\Settings;
+
+use dhope0000\LXDClient\Objects\Host;
+use dhope0000\LXDClient\Tools\User\ValidatePermissions;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UpdateHostSettingsController implements \dhope0000\LXDClient\Interfaces\RecordAction
+{
+    private $validatePermissions;
+
+    public function __construct(ValidatePermissions $validatePermissions)
+    {
+        $this->validatePermissions = $validatePermissions;
+    }
+    /**
+     * @Route("", name="Get hosts settings")
+     */
+    public function update($userId, Host $host, array $settings)
+    {
+        $this->validatePermissions->isAdminOrThrow($userId);
+        $info = $host->host->info();
+        foreach ($settings as $key=>$value) {
+            $info["config"][$key] = $value;
+        }
+        $host->host->replace($info['config']);
+        return ["state"=>"success", "messages"=>"Updated LXD Settings"];
+    }
+}
