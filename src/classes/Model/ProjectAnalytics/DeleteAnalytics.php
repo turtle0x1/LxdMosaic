84f0bc442f6b3fe12b3d30ff28c9c492cd6cef5f
turtle0x1 <dhope0000@gmail.com>
2022-09-09 17:40:25 +0100

    Deleting host removes to many metrics

--- src/classes/Model/ProjectAnalytics/DeleteAnalytics.php
+++ src/classes/Model/ProjectAnalytics/DeleteAnalytics.php
@@ -7,7 +7,7 @@ use dhope0000\LXDClient\Model\Database\Database;
 class DeleteAnalytics
 {
     private $database;
-    
+
     public function __construct(Database $database)
     {
         $this->database = $database->dbObject;
@@ -32,7 +32,7 @@ class DeleteAnalytics
         $sql = "DELETE FROM
                     `Project_Analytics`
                 WHERE
-                    `PA_Host_ID` <= :hostId
+                    `PA_Host_ID` = :hostId
                 ";
         $do = $this->database->prepare($sql);
         $do->execute([
