f0550296ba3f97e10f525956ab859513f68dc0fd
turtle0x1 <dhope0000@gmail.com>
2025-10-02 22:34:52 +0100

    Another env path fix
    (Should have branched, sigh)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,6 @@
 
 require __DIR__ . "/../vendor/autoload.php";
 
-$env = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$env = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $env->load();
 $env->required(['DB_HOST', 'DB_USER', 'DB_PASS', 'DB_NAME']);
