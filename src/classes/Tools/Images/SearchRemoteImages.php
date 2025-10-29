3c582ae76b3407608f9ee9655a9f3a503454f8bd
turtle0x1 <dhope0000@gmail.com>
2025-10-29 19:50:14 +0000

    Fix issue with searching remote images

--- src/classes/Tools/Images/SearchRemoteImages.php
+++ src/classes/Tools/Images/SearchRemoteImages.php
@@ -45,7 +45,7 @@ class SearchRemoteImages
             }
 
 
-            if ($imArch !== $searchArch) {
+            if ($searchArch !== "" && $imArch !== $searchArch) {
                 continue;
             }
 
